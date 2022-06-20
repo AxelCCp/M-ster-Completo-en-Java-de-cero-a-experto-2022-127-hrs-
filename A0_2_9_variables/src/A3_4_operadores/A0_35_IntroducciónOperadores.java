031563a514ed2b67a9d95b4a0a72b8f3c1cda313
msi <hpmajin@outlook.es>
2022-06-19 22:24:55 -0400

    hasta la clase 41

---
+++ A0_2_9_variables/src/A3_4_operadores/A0_35_IntroducciónOperadores.java
@@ -0,0 +1,51 @@
+package A3_4_operadores;
+
+public class A0_35_IntroducciónOperadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		//EXISTEN LOS OPERADORES : 
+			
+			//ARITMÉTICOS  		+ - / * %
+			//RELACIONALES		< > >= <= == != 
+			//LÓGICOS			AND : && ...  OR : || ... NOT : !  ... AND: & ... OR : | ... XOr ^   (SOMBRERO : SE USA A NIVEL DE BITS A^B = ES TRUE CUANDO A Y B SON !=)
+			
+								/*TABLA
+								 * A		B		AND 		OR		NOT		XOr
+								 * true		true	true		true	false	0 
+								 * true		false	false		true	false	1
+								 * false	true	false		true	false	1 
+								 * false	false 	false		false	true	0
+								 */
+		
+		
+			//ASIGNACIÓN
+		
+		
+		//OTROS OPERADORES : 
+	
+			//UNARIOS  : 		-(3+4)
+			//COMBINADOS : 	    -= += /= *=
+			
+			//INCREMENTALES : 
+				// POST :		a++ a-- 
+				// PRE  :		++a --a
+		
+			//TERNARIOS :		(CONDICIÓN)? valor1 : valor2
+				
+				
+		 	//COMBINADOS - DE BITS - DE INSTANCIA
+				
+				
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ A0_2_9_variables/src/A3_4_operadores/A3_38_OperadoresUnarios.java
@@ -0,0 +1,23 @@
+package A3_4_operadores;
+
+public class A3_38_OperadoresUnarios {
+
+	public static void main(String[]args) {
+		
+		
+		//EXSISTEN 2 OPERADORES UNARIOS, EL   "+"  	Y EL   "-"
+		//ESTOS INVIERTEN EL SIGNO DEL NÚMERO
+		
+		//  - * - = +
+		//  + * - = -
+		
+		int i = -5;
+		int j = +i;    // j = i*1 = -5
+		System.out.println(j);  											// R : -5
+		
+		int k = -i;    // k = -5*-1 = -(-5) = 5
+		System.out.println(k);												// R : 5
+		
+	}	
+	
+}
